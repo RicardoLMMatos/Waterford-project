44ceb0a3906c7cb5ab5ff06541f2fdb95a6ffbdc
Ricardo Matos <ricardomatos_976@hotmail.com>
2024-04-13 13:13:54 +0000

    modified slick carousel images to fit the container 100%

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
 //This piece of javascript code was copied from w3schools website and modified by me
 $(document).ready(function () {
 
-    let $reviewsSlick = $('.reviews-slick');
+    let $reviewsSlick = $('.function-photos-slick');
     if ($reviewsSlick.length > 0) {
       $reviewsSlick.slick({
+        'infinite' : true,
         'arrows': false,
         'dots': false,
         'autoplay': true,
-        'autoplaySpeed': 3000,
-        'adaptiveHeight': true
+        'autoplaySpeed': 4000,
+        'adaptiveHeight': false
       });
     }
    
